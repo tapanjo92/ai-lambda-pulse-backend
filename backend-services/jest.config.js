0211ef091ea82a4ddebc82abcb8433db0f756aa0
root <root@ip-10-120-0-183.ap-south-1.compute.internal>
2025-05-30 08:32:58 +0000

    test script

---
+++ backend-services/jest.config.js
@@ -0,0 +1,9 @@
+// backend-services/jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // Optional: You can add other Jest configurations here if needed,
+  // for example, similar to your backend-infra/jest.config.js:
+  // testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  // moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+};
